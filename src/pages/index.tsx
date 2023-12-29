82bbef400701b7937707d9f79e2ec4fb4ac5b0af
Hunter Wyatt <hunterwyatt@Hunters-Mac-mini.local>
2023-12-29 09:39:43 -0700

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,190 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-const inter = Inter({ subsets: ['latin'] })
+const songList = [
+  {
+    id: "gary",
+    name: "Gary Goodspeed",
+    thumb:
+      "https://upload.wikimedia.org/wikipedia/en/a/a5/Graves_into_Gardens_by_Elevation_Worship_%28Official_Album_Cover%29.png",
+    bpm: 128,
+    timeSignature: "4/4",
+    hasTrack: false,
+    key: "A",
+    duration: "4:32",
+  },
+  {
+    id: "cato",
+    name: "Little Cato",
+    thumb:
+      "https://upload.wikimedia.org/wikipedia/en/a/a5/Graves_into_Gardens_by_Elevation_Worship_%28Official_Album_Cover%29.png",
+    bpm: 128,
+    timeSignature: "6/8",
+    hasTrack: true,
+    key: "A",
+    duration: "4:32",
+  },
+  {
+    id: "kvn",
+    name: "KVN",
+    thumb:
+      "https://upload.wikimedia.org/wikipedia/en/a/a5/Graves_into_Gardens_by_Elevation_Worship_%28Official_Album_Cover%29.png",
+    bpm: 128,
+    timeSignature: "3/4",
+    hasTrack: false,
+    key: "A",
+    duration: "4:32",
+  },
+  {
+    id: "mooncake",
+    name: "Mooncake",
+    thumb:
+      "https://upload.wikimedia.org/wikipedia/en/a/a5/Graves_into_Gardens_by_Elevation_Worship_%28Official_Album_Cover%29.png",
+    bpm: 128,
+    timeSignature: "4/4",
+    hasTrack: false,
+    key: "A",
+    duration: "4:32",
+  },
+  {
+    id: "quinn",
+    name: "Quinn Ergon",
+    thumb:
+      "https://upload.wikimedia.org/wikipedia/en/a/a5/Graves_into_Gardens_by_Elevation_Worship_%28Official_Album_Cover%29.png",
+    bpm: 128,
+    timeSignature: "4/4",
+    hasTrack: true,
+    key: "A",
+    duration: "4:32",
+  },
+];
 
 export default function Home() {
+  const [songs, updateSongs] = useState(songList);
+
+  function handleOnDragEnd(result: any) {
+    if (!result.destination) return;
+
+    const items = Array.from(songs);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    updateSongs(items);
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-gray-600 h-screen p-5">
+      <div style={{ maxWidth: "1280px" }} className="mx-auto">
+        <div className={"pb-5 border-b"}>
+          <header className="App-header">
+            <h1
+              className={`text-4xl font-semibold uppercase pb-3 mb-5 border-b`}
+            >
+              Sunday Set Builder
+            </h1>
+          </header>
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable droppableId="songs">
+              {(provided) => (
+                <ul
+                  className="flex flex-col gap-4 list-none"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {songs.map(
+                    (
+                      {
+                        id,
+                        name,
+                        thumb,
+                        bpm,
+                        timeSignature,
+                        hasTrack,
+                        key,
+                        duration,
+                      },
+                      index
+                    ) => {
+                      return (
+                        <Draggable key={id} draggableId={id} index={index}>
+                          {(provided) => (
+                            <li
+                              className={`relative flex items-center bg-white p-2 border-2 border-gray-200 rounded`}
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              <div className="h-32 w-32 mr-4 shrink-0">
+                                <img
+                                  className={`w-full h-auto`}
+                                  src={thumb}
+                                  alt={`${name} Thumb`}
+                                />
+                              </div>
+                              <div className={"flex flex-col gap-2"}>
+                                <p className={`text-md font-bold text-black`}>
+                                  {name}
+                                </p>
+                                <div
+                                  className={
+                                    "flex flex-col gap-1 justify-content-center"
+                                  }
+                                >
+                                  <p
+                                    className={`text-xs font-semibold text-gray-500`}
+                                  >
+                                    Key: {key}
+                                  </p>
+                                  <p
+                                    className={`text-xs font-semibold text-gray-500`}
+                                  >
+                                    BPM: {bpm}
+                                  </p>
+                                  <p
+                                    className={`text-xs font-semibold text-gray-500`}
+                                  >
+                                    Time Sig: {timeSignature}
+                                  </p>
+                                  <p
+                                    className={`text-xs font-semibold text-gray-500`}
+                                  >
+                                    Duration: {duration}
+                                  </p>
+                                </div>
+                              </div>
+                              {/* Edit button */}
+                              <button
+                                className="absolute top-0 right-0 p-2 mr-2 text-gray-500 hover:text-gray-700"
+                                onClick={() => console.log(`edit ${name}`)}
+                              >
+                                Edit
+                              </button>
+                            </li>
+                          )}
+                        </Draggable>
+                      );
+                    }
+                  )}
+                  {provided.placeholder}
+                </ul>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </div>
+        <div className={"flex justify-between gap-4 pt-5"}>
+          <div className={"flex gap-4"}>
+            <button className={"bg-white text-gray-800 font-bold px-4 py-3"}>
+              Add Click Track
+            </button>
+            <button className={"bg-white text-gray-800 font-bold px-4 py-3"}>
+              Upload Track
+            </button>
+          </div>
+          <button className={"bg-white text-gray-800 font-bold px-4 py-3"}>
+            Build Ableton Set
+          </button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }

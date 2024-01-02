b95ab10b7f7d3a310cb530ff35988f86e4bc94db
Hunter Wyatt <hunterwyatt@Hunters-Mac-mini.local>
2024-01-01 21:02:37 -0700

    add displayKey to edit modal

--- src/components/EditSongModal.tsx
+++ src/components/EditSongModal.tsx
@@ -52,7 +52,7 @@ const EditSongModal = ({ onClose, onEditSong, song }: any) => {
             <option value="-3">-3</option>
             <option value="-2">-2</option>
             <option value="-1">-1</option>
-            <option value="0">0</option>
+            <option value="0">0 ({song.displayKey})</option>
             <option value="1">+1</option>
             <option value="2">+2</option>
             <option value="3">+3</option>

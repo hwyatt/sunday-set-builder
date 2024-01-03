1bf0b7d1dd24523ddd230abda298986424824ce5
Hunter Wyatt <hunterwyatt@Hunters-Mac-mini.local>
2024-01-02 20:52:43 -0700

    meta test 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Head } from "next/document";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -19,23 +18,6 @@ export default function App({ Component, pageProps }: AppProps) {
           gtag('config', 'G-N1F9EJ6CY4');
         `}
       </Script>
-      <Head>
-        <meta
-          property="og:title"
-          content="Build an Ableton Set in 60 seconds"
-          key="ogtitle"
-        />
-        <meta
-          property="og:description"
-          content="Sunday Set Builder - a tool for Worship Pastors"
-          key="ogdesc"
-        />
-        <meta
-          property="og:image"
-          content="https://i.ibb.co/ySBt1Jm/SSB-Logo.png"
-          key="ogimage"
-        />
-      </Head>
       <Component {...pageProps} />
     </>
   );
